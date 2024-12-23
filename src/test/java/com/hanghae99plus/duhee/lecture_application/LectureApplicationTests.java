45473bd7b0e17111bef4ec4c879f8004f4e91025
diocto <duhee74@naver.com>
2024-12-23 20:52:57 +0900

    Initial commit

---
+++ src/test/java/com/hanghae99plus/duhee/lecture_application/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99plus.duhee.lecture_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
